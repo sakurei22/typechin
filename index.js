6101be68a671556a7392ffd64e9733166df1b6b3
sj <sakurei22@gmail.com>
2022-04-13 11:40:57 +0900

    first

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello ts World");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "sujin", age = 22;
+const sayHi = (name, age) => {
+    console.log(`Hello ${name}, you are ${age}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello ts World");
+const name = "sujin",
+age = 22;
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+
+export {};
