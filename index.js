b6c711b1e6e24591074e622c18fbbe81223b7a61
sj <sakurei22@gmail.com>
2022-04-19 09:12:17 +0900

    interface

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "sujin", age = 22;
-const sayHi = (name, age) => {
-    console.log(`Hello ${name}, you are ${age}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
